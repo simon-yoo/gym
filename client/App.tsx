f12629feed472846df1e8a3b3d11810479e6feb7
Simon Yoo <yoosghn0@gmail.com>
2023-08-26 16:47:53 +1200

    fixed css

--- client/App.tsx
+++ client/App.tsx
@@ -14,6 +14,7 @@ import Contact from './components/Contact'
 
 
   function App(){
+    
     return (
         <RouterProvider
         router={createBrowserRouter(
--- client/components/Location.tsx
+++ client/components/Location.tsx
@@ -2,79 +2,112 @@ const Location = () => {
     return (
         <>
         <section className="photos">
-        <hr className="cut-line"/>
-        <div className="row">
-            <div className="column">
+        {/* <hr className="cut-line"/> */}
+        {/* <div className="row"> */}
+            <div className="column-location">
                 <h3 className="city">New York</h3>
-                <img className="city-img" src="photos/location/newyork.jpeg"/>
-                <span><b>Address:</b> 123 avcd road, New York, USA <br/><b>Ph: </b>0123456789 <br/> <b>E.mail:</b>
+                <img className="city-img" src="/client/public/photos/location/newyork.jpeg"/>
+                <div className="location-details">
+                    <div><b>Address:</b> 123 avcd road, Tokyo, USA </div>
+                    <div><b>Ph: </b>0123456789</div>
+                    <div><b>E.mail:</b>
                     abcd123@abc.com
-                    </span>
-            </div>
-            <div className="column">
+                    </div>     
+                </div>
+                </div>
+            <div className="column-location">
                 <h3 className="city">Los Angeles</h3>
-                <img className="city-img" src="photos/location/losangeles.avif"/>
-                <span><b>Address:</b> 123 avcd road, Los Angeles, USA <br/><b>Ph: </b>0123456789 <br/> <b>E.mail:</b>
+                <img className="city-img" src="/client/public/photos/location/losangeles.avif"/>
+                
+                <div className="location-details">
+                <div><b>Address:</b> 123 avcd road, Tokyo, USA </div>
+                <div><b>Ph: </b>0123456789</div>
+                <div><b>E.mail:</b>
                     abcd123@abc.com
-                    </span>
+                </div>     
+                </div>
             </div>
-            <div className="column">
-                <h3 className="citys">California</h3>
-                <img className="city-img" src="photos/location/california.avif"/>
-                <span><b>Address:</b> 123 avcd road, California, USA <br/><b>Ph: </b>0123456789 <br/> <b>E.mail:</b>
+            <div className="column-location">
+                <h3 className="city">California</h3>
+                <img className="city-img" src="/client/public/photos/location/california.avif"/>
+                <div className="location-details">
+                <div><b>Address:</b> 123 avcd road, Los Angeles, USA </div>
+                <div><b>Ph: </b>0123456789</div>
+                <div><b>E.mail:</b>
                     abcd123@abc.com
-                    </span>
+                </div>     
+                </div>
             </div>
-        </div>
-        <hr className="cut-line"/>
-        <div className="row">
-            <div className="column">
+        {/* </div> */}
+        {/* <div className="row"> */}
+            <div className="column-location">
                 <h3 className="city">Seoul</h3>
-                <img className="city-img" src="photos/location/seoul3.jpeg"/>
-                <span><b> Address:</b> 123 avcd road, Seoul, South Korea<br/> <b> Ph:</b> 0123456789 <br/> <b> E.mail:</b>
+                <img className="city-img" src="/client/public/photos/location/seoul3.jpeg"/>
+                <div className="location-details">
+                <div><b>Address:</b> 123 avcd road, Seoul, USA </div>
+                <div><b>Ph: </b>0123456789</div>
+                <div><b>E.mail:</b>
                     abcd123@abc.com
-                </span>
+                </div>     
+                </div>
             </div>
-            <div className="column">
+            <div className="column-location">
                 <h3 className="city">Tokyo</h3>
-                <img className="city-img" src="photos/location/tokyo.avif"/>
-                <span><b>Address:</b> 123 avcd road, Tokyo, Japan <br/><b>Ph: </b>0123456789 <br/> <b>E.mail:</b>
+                <img className="city-img" src="/client/public/photos/location/tokyo.avif"/>
+                <div className="location-details">
+                <div><b>Address:</b> 123 avcd road, Tokyo, USA </div>
+                <div><b>Ph: </b>0123456789</div>
+                <div><b>E.mail:</b>
                     abcd123@abc.com
-                </span>
+                </div>     
+                </div>
             </div>
-            <div className="column">
+            <div className="column-location">
                 <h3 className="city">Singapore</h3>
-                <img className="city-img" src="photos/location/singapore.avif"/>
-                <span><b>Address:</b> 123 avcd road, Singapore, Singapore <br/><b>Ph: </b>0123456789 <br/> <b>E.mail:</b>
+                <img className="city-img" src="/client/public/photos/location/singapore.avif"/>
+                <div className="location-details">
+               <div><b>Address:</b> 123 avcd road, Singapore, USA </div>
+                <div><b>Ph: </b>0123456789</div>
+                <div><b>E.mail:</b>
                     abcd123@abc.com
-                </span>
+                </div>     
+               </div>
             </div>
-        </div>
-        <hr className="cut-line"/>
-        <div className="row">
-            <div className="column">
+        {/* </div> */}
+        {/* <div className="row"> */}
+            <div className="column-location">
                 <h3 className="city">Paris</h3>
-                <img className="city-img" src="photos/location/paris1.jpeg"/>
-                <span><b>Address:</b> 123 avcd road, Paris, France<br/><b>Ph: </b>0123456789 <br/> <b>E.mail:</b>
+                <img className="city-img" src="/client/public/photos/location/paris1.jpeg"/>
+                <div className="location-details"> 
+                <div><b>Address:</b> 123 avcd road, Paris, USA </div>
+                <div><b>Ph: </b>0123456789</div>
+                <div><b>E.mail:</b>
                     abcd123@abc.com
-                    </span>
+                </div>     </div>
             </div>
-            <div className="column">
+            <div className="column-location">
                 <h3 className="city">London</h3>
-                <img className="city-img" src="photos/location/london.avif"/>
-                <span><b>Address:</b> 123 avcd road, London, United Kingdon <br/><b>Ph: </b>0123456789 <br/>
-                    <b>E.mail:</b>
+                <img className="city-img" src="/client/public/photos/location/london.avif"/>
+                <div className="location-details">
+                <div><b>Address:</b> 123 avcd road, London, USA </div>
+                <div><b>Ph: </b>0123456789</div>
+                <div><b>E.mail:</b>
                     abcd123@abc.com
-                    </span>
+                </div>     
+                </div>
             </div>
-            <div className="column">
+            <div className="column-location">
                 <h3 className="city">Madrid</h3>
-                <img className="city-img" src="photos/location/madrid.avif"/>
-                <span><b>Address:</b> 123 avcd road, Madrid, Spain<br/><b>Ph: </b>0123456789 <br/> <b>E.mail:</b>
+                <img className="city-img" src="/client/public/photos/location/madrid.avif"/>
+                <div className="location-details">
+               <div><b>Address:</b> 123 avcd road, Madrid, USA </div>
+                <div><b>Ph: </b>0123456789</div>
+                <div><b>E.mail:</b>
                     abcd123@abc.com
-                    </span>
+                </div>     
+               </div>
             </div>
-        </div>
+        {/* </div> */}
     </section>
     </>
 
