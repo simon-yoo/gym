dac72d31d417124c9b86c000fb1121b67f018389
Simon Yoo <yoosghn0@gmail.com>
2023-10-02 18:26:53 +1300

    nvm

---
+++ server/server.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+import * as Path from 'node:path'
+const server = express()
+
+server.use(express.json())
+
+if (process.env.NODE_ENV === 'production') {
+  server.use(express.static(Path.resolve('public')))
+  server.use('/assets', express.static(Path.resolve('./dist/assets')))
+  server.get('*', (req, res) => {
+    res.sendFile(Path.resolve('./dist/index.html'))
+  })
+}
+
+export default server
